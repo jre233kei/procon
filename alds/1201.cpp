3a38c2e86dffcd6d1944d2edb392bb5b6a514c24
福井航大 <jre233kei+github@gmail.com>
2019-08-24 23:41:15 +0900

    [Add] ALDS Section 11

---
+++ alds/1201.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+static const int N = 100;
+
+int main() {
+    int M[N][N];
+    int n, u, k, v;
+
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        for (int j = 0; j < n; ++j) {
+            M[i][j] = 0;
+        }
+    }
+    
+
+}
